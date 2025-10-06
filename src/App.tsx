eb5a3cfc761cc9e4a95ca88b4d8fd0dda0e3612e
vincent <1339301794@qq.com>
2025-10-06 20:56:43 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+ import GeoWorld3 from './GeoWorld/index';
+   
 function App() {
+
+  useEffect(() => {
+
+const dom: HTMLElement = document.querySelector('#earth-canvas')!;
+new GeoWorld3({
+  dom
+});
+
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="earth-canvas">
+      <div id="tooltip">这是什么</div>
     </div>
   );
 }
